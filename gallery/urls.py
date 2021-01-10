f034c4c2ca667c486d982bcbbb6034aa46a30fe1
Elisephan Ntakirutimana <elise.ntakirutimana@gmail.com>
2021-01-10 10:36:46 +0200

    add changes

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.static import static
 
 urlpatterns=[
     
-    url(r'^$',views.mygallery,name='pics'),
+    url(r'^$',views.mygallery,name='gallery'),
     url(r'^search/', views.search_results, name='search_results'),
 ]
 if settings.DEBUG:
