0151f7715ae6325e1b229ac69071ea19afde6b9a
Elisephan Ntakirutimana <elise.ntakirutimana@gmail.com>
2021-01-11 15:59:52 +0200

    add changes

--- kigali/settings.py
+++ kigali/settings.py
@@ -164,7 +164,7 @@ STATICFILES_DIRS = (
 
 # configuring the location for media
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
